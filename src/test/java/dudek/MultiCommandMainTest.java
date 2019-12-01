fa5e1431a8d4365f865f8793c4f6b3d7045fc9a6
Marek Dudek <marek.dudek@gmail.com>
2019-12-01 17:19:45 +0100

    smoke test for jinfo file Java 11

--- src/test/java/dudek/MultiCommandMainTest.java
+++ src/test/java/dudek/MultiCommandMainTest.java
@@ -1,6 +1,7 @@
 package dudek;
 
 import dudek.commands.Install;
+import dudek.commands.JInfo;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
@@ -14,6 +15,18 @@ final class MultiCommandMainTest {
         final Object command = new Install();
         final CommandLine cmd = new CommandLine(command);
         final String[] args = {"/lib/jvm/jdk-11.0.5/", "100"};
+        // when
+        final int code = cmd.execute(args);
+        // then
+        assertThat(code).isEqualTo(0);
+    }
+
+    @Test
+    void jinfo_file_11() {
+        // given
+        final Object command = new JInfo();
+        final CommandLine cmd = new CommandLine(command);
+        final String[] args = {"/lib/jvm/jdk-11.0.5/", "jdk-11", "100"};
         // when
         final int code = cmd.execute(args);
         // then
