6cfeaf41089fd8fea6aabc4b83d1d579c56d03a2
Marek Dudek <marek.dudek@gmail.com>
2019-12-01 17:23:11 +0100

    smoke tests for Java 8

--- src/test/java/dudek/MultiCommandMainTest.java
+++ src/test/java/dudek/MultiCommandMainTest.java
@@ -10,11 +10,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 final class MultiCommandMainTest {
 
     @Test
+    void install_8() {
+        // given
+        final Object command = new Install();
+        final CommandLine cmd = new CommandLine(command);
+        final String[] args = {"/lib/jvm/jdk1.8.0_221/", "200"};
+        // when
+        final int code = cmd.execute(args);
+        // then
+        assertThat(code).isEqualTo(0);
+    }
+
+    @Test
     void install_11() {
         // given
         final Object command = new Install();
         final CommandLine cmd = new CommandLine(command);
         final String[] args = {"/lib/jvm/jdk-11.0.5/", "100"};
+        // when
+        final int code = cmd.execute(args);
+        // then
+        assertThat(code).isEqualTo(0);
+    }
+
+    @Test
+    void jinfo_file_8() {
+        // given
+        final Object command = new JInfo();
+        final CommandLine cmd = new CommandLine(command);
+        final String[] args = {"/lib/jvm/jdk1.8.0_221/", "jdk-8", "200"};
         // when
         final int code = cmd.execute(args);
         // then
